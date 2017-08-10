26a7623bdbbdcbb688ca4d528c9d26d83bca2f91
Your Namgit config --global user.email you@example.come <you@example.com>
2017-08-10 22:07:03 +0800

    Initial Commit

---
+++ AlertTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AlertTool
+//
+//  Created by X on 2017/8/10.
+//  Copyright © 2017年 X. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AlertTool/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AlertTool
+//
+//  Created by X on 2017/8/10.
+//  Copyright © 2017年 X. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
